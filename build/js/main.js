d9aac90af74654ebfd9c34d3a316c15efbfce070
Egor <egor_eremin@mail.ru>
2019-06-06 18:20:19 +0800

    full static page

--- build/js/main.js
+++ build/js/main.js
@@ -176,6 +176,19 @@ $(document).ready(function () {
     });
   })();
 
+  (function addNumberAfterFocus() {
+    $('.input-phone').on('focus', function () {
+      if (!$(this).val()) {
+        $(this).val('+7 (');
+      }
+    });
+    $('.input-phone').on('blur', function () {
+      if ($(this).val().length <= 4) {
+        $(this).val('');
+      }
+    });
+  })();
+
   (function addPhoneMask() {
     $('.input-phone').mask('+7 (000) 000-00-00', {
       placeholder: "+7 (___) ___-__-__"
@@ -183,29 +196,65 @@ $(document).ready(function () {
   })();
 
   (function validationInputForm() {
-    validationForm('.product-selection', '.form-item-wrapper', '.good-text-wrapper');
+    validationForm('.product-selection', '.form-item-wrapper', '.good-text-wrapper', true);
   })();
 
   (function goPrev() {
-    $('.prev').on('click', function () {
-      var currentPhone = $(this).parent('.attention-block__text-wrapper').find('.current-phone');
-      $('.input-phone').val('');
+    $('.prev').on('click', function (e) {
+      e.preventDefault();
       $('.good-text-wrapper').removeClass('show-information');
       $('.form-item-wrapper').removeClass('hide-information');
+      $('.input-phone').focus();
     });
+  })();
+
+  (function mapInit() {
+    if ($('#map-init').length > 0) {
+      var init = function init() {
+        var myMap = new ymaps.Map('map-init', {
+          center: [52.278407791991356, 104.21523449667427],
+          zoom: 16,
+          controls: []
+        }, {
+          suppressMapOpenBlock: true,
+          searchControlProvider: 'yandex#search'
+        }),
+            myPlacemark = window.myPlacemark = new ymaps.Placemark([52.278407791991356, 104.21523449667427], {}, {
+          balloonShadow: false,
+          iconLayout: 'default#image',
+          iconImageHref: '../images/point.svg',
+          iconImageSize: [29, 48],
+          iconImageOffset: [-14, -48]
+        });
+        myMap.geoObjects.add(myPlacemark);
+        myMap.behaviors.disable('scrollZoom'); // media ('all and (min-width: 1025px)', function () {
+        //     myMap.setCenter([52.278407791991356,104.21523449667427]);
+        // })
+        // media ('all and (max-width: 1024px)', function () {
+        //     myMap.setCenter([52.278407791991356,104.21523449667427]);
+        // })
+      };
+
+      ymaps.ready(init);
+    }
   })();
 });
 
-function validationForm(formInit, formWrapper, textGood, textBad) {
+function validationForm(formInit, formWrapper, textGood, animation) {
   $(formInit).validate({
     invalidHandler: function invalidHandler(e, v) {
-      v.errorContext.addClass('animate');
+      if (animation) {
+        var animationForm = v.errorContext;
+        animationForm.addClass('animation');
+        setTimeout(function () {
+          animationForm.removeClass('animation');
+        }, 400);
+      }
     },
     submitHandler: function submitHandler(form) {
       var inputPhone = $(form).find('.user-phone');
       var inputValue = inputPhone.val();
       $(textGood).find('.current-phone').text(inputValue);
-      $(form).removeClass('animate');
       $.ajax({
         type: $(form).attr('method'),
         url: $(form).attr('action'),
@@ -219,7 +268,7 @@ function validationForm(formInit, formWrapper, textGood, textBad) {
           $(textGood).addClass('show-information');
         },
         error: function error() {
-          console.log('Упс... Что-то пошло по пизде!');
+          console.log('Упс... Что-то пошло не так!');
         }
       });
       return false;
--- src/js/main.js
+++ src/js/main.js
@@ -5,41 +5,100 @@ $(document).ready(function () {
         var elementParent = $('.input-phone').parent('.form-item');
         $('.input-phone').focus(function () {
             elementParent.addClass('focus');
+
         });
 
         $('.input-phone').blur(function () {
             elementParent.removeClass('focus');
         });
     })();
+    (function addNumberAfterFocus() {
+        $('.input-phone').on('focus', function () {
+            if (!$(this).val()) {
+                $(this).val('+7 (');
+            }
+        });
+        $('.input-phone').on('blur', function () {
+            if ($(this).val().length <= 4) {
+                $(this).val('');
+            }
+        });
+    })();
     (function addPhoneMask() {
-        $('.input-phone').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+        $('.input-phone').mask('+7 (000) 000-00-00', {
+            placeholder: "+7 (___) ___-__-__",
+            });
     })();
     (function validationInputForm() {
-        validationForm('.product-selection', '.form-item-wrapper', '.good-text-wrapper');
+        validationForm('.product-selection', '.form-item-wrapper', '.good-text-wrapper', true);
     })();
     (function goPrev() {
-        $('.prev').on('click', function () {
-            var currentPhone = $(this).parent('.attention-block__text-wrapper').find('.current-phone');
+        $('.prev').on('click', function (e) {
+            e.preventDefault();
 
-            $('.input-phone').val('');
             $('.good-text-wrapper').removeClass('show-information');
             $('.form-item-wrapper').removeClass('hide-information');
+            $('.input-phone').focus();
         });
+    })();
+    (function mapInit() {
+        if ($('#map-init').length > 0) {
+
+            ymaps.ready(init);
+
+            function init() {
+                var myMap = new ymaps.Map('map-init', {
+                        center: [52.278407791991356,104.21523449667427],
+                        zoom: 16,
+                        controls: []
+                    }, {
+                        suppressMapOpenBlock: true,
+                        searchControlProvider: 'yandex#search'
+                    }),
+
+                    myPlacemark = window.myPlacemark = new ymaps.Placemark([52.278407791991356,104.21523449667427], {
+                    }, {
+                        balloonShadow: false,
+                        iconLayout: 'default#image',
+                        iconImageHref: '../images/point.svg',
+                        iconImageSize: [29, 48],
+                        iconImageOffset: [-14, -48]
+                    });
+
+
+                myMap.geoObjects.add(myPlacemark);
+                myMap.behaviors.disable('scrollZoom');
+
+
+                // media ('all and (min-width: 1025px)', function () {
+                //     myMap.setCenter([52.278407791991356,104.21523449667427]);
+                // })
+                // media ('all and (max-width: 1024px)', function () {
+                //     myMap.setCenter([52.278407791991356,104.21523449667427]);
+                // })
+
+            }
+        }
     })();
 
 });
 
-function validationForm(formInit, formWrapper, textGood, textBad) {
+function validationForm(formInit, formWrapper, textGood, animation) {
     $(formInit).validate({
         invalidHandler: function (e, v) {
-            v.errorContext.addClass('animate');
+            if (animation) {
+                var animationForm = v.errorContext;
+                animationForm.addClass('animation');
+                setTimeout(function () {
+                    animationForm.removeClass('animation');
+                }, 400);
+            }
         },
         submitHandler: function(form) {
             var inputPhone = $(form).find('.user-phone');
             var inputValue = inputPhone.val();
 
             $(textGood).find('.current-phone').text(inputValue);
-            $(form).removeClass('animate');
             $.ajax({
                 type: $(form).attr('method'),
                 url: $(form).attr('action'),
@@ -55,7 +114,7 @@ function validationForm(formInit, formWrapper, textGood, textBad) {
                         $(textGood).addClass('show-information');
                 },
                 error: function() {
-                    console.log('Упс... Что-то пошло по пизде!');
+                    console.log('Упс... Что-то пошло не так!');
                 }
             });
             return false;
